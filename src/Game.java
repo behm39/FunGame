74623823890a1e06f618afd7eedb2dc7fa629175
Michael Behm <behm.39@buckeyemail.osu.edu>
2018-04-23 16:34:40 -0400

    Fixed Errors

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,12 @@
+import java.awt.Canvas;
 
-public class Game extends Canvas, implements Runnable{
+@SuppressWarnings("serial")
+public class Game extends Canvas implements Runnable{
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
