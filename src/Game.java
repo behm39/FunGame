7780f36f100eaed640c4a79e4161991202e0cf70
Michael Behm <behm.39@buckeyemail.osu.edu>
2018-04-23 16:59:46 -0400

    Stubbed in the Game class

--- src/Game.java
+++ src/Game.java
@@ -3,10 +3,37 @@ import java.awt.Canvas;
 @SuppressWarnings("serial")
 public class Game extends Canvas implements Runnable{
 
+	public Game() {
+		
+	}
+	
+	public void start() {
+		
+	}
+	
+	public void stop() {
+		
+	}
+	
+	public void tick() {
+		
+	}
+	
+	public void render() {
+		
+		
+	}
+
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
+
+	}
+	
+	public static void main(String[] args) {
 		
 	}
+	
+	
 
 }
